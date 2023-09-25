f2a89d1e4646022ba417e7f181f0c53766813675
swadebsharma6 <swadebriva2012@gmail.com>
2023-09-25 10:54:54 +0600

    added handleremove from localstorage

---
+++ src/Pages/FavPhoneCard/FavPhoneCard.jsx
@@ -0,0 +1,59 @@
+import PropTypes from 'prop-types';
+
+const FavPhoneCard = ({phone}) => {
+    // console.log(phone);
+    const { image, phone_name,brand_name, price,} = phone ;
+
+    return (
+        <div className='m-3'>
+        <div className="relative flex w-full flex-row rounded-xl bg-white bg-clip-border text-gray-700 shadow-md border p-6">
+        <div className="relative m-0 w-2/5 shrink-0 overflow-hidden rounded-xl rounded-r-none bg-white bg-clip-border text-gray-700">
+          <img
+            src={image}
+            alt="image"
+            className="h-full w-full object-cover"
+          />
+        </div>
+        <div className="p-6">
+          <h6 className="mb-4 block font-sans text-base font-semibold uppercase leading-relaxed tracking-normal text-pink-500 antialiased">
+            {phone_name} --- ${price}
+          </h6>
+          <h4 className="mb-2 block font-sans text-2xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
+           {brand_name}
+          </h4>
+          
+          <a className="inline-block" href="#">
+            <button
+              className="flex select-none items-center gap-2 rounded-lg py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-pink-500 transition-all hover:bg-pink-500/10 active:bg-pink-500/30 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+              type="button"
+            >
+              View Details
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="2"
+                stroke="currentColor"
+                aria-hidden="true"
+                className="h-4 w-4"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
+                ></path>
+              </svg>
+            </button>
+          </a>
+        </div>
+      </div> 
+        </div>
+    );
+};
+
+
+FavPhoneCard.propTypes = {
+    phone: PropTypes.object,
+    
+}
+export default FavPhoneCard;
--- src/Pages/Favorites/Favorites.jsx
+++ src/Pages/Favorites/Favorites.jsx
@@ -1,12 +1,45 @@
-import FavoritePhn from "./FavoritePhn";
-
+import { useEffect, useState } from 'react';
+import FavPhoneCard from '../FavPhoneCard/FavPhoneCard';
 
 const Favorites = () => {
+    const[favorites, setFavorites] = useState([]) ;
+    const[noData, setNoData] = useState('');
 
+
+    useEffect(()=>{
+        
+        const favoriteItems = JSON.parse(localStorage.getItem('favorites'));
+       if(favoriteItems){
+        setFavorites(favoriteItems)
+       }
+       else{
+        setNoData('No data Found')
+       }
+
+    }, []);
+    // console.log(favorites)
+
+    const handleRemove =()=>{
+        localStorage.clear();
+        setFavorites([]);
+        setNoData('No data Found Here')
+    }
 
     return (
         <div>
-           <FavoritePhn></FavoritePhn>
+          {
+            noData ? <p className='h-[80vh] flex justify-center items-center text-xl font-semibold text-red-600'>{noData} !!</p> : <div>
+
+                {favorites.length > 0 &&  <button onClick={handleRemove} className="btn btn-wide block mx-auto my-10">Delete All favorites</button>}
+                <div className='grid md:grid-cols-2 gap-4'>
+                {favorites.map(phone => <FavPhoneCard
+                    key={phone.id}
+                    phone={phone}
+                    ></FavPhoneCard> )}
+                </div>
+
+            </div>
+          }
         </div>
     );
 };
